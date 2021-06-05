d5c62ac0909d25d81e93feb53f1ba18a0e6a5609
SeungYongShim <syshim@me.com>
2021-06-05 22:42:55 +0900

    샘플 추가

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -9,9 +9,36 @@ namespace ConsoleApp1
     {
         private static async Task Main()
         {
-            var ret = await Task.FromResult(1).SelectMany(x => Task.FromResult(x.ToString()));
+            var ret1 = Task.FromResult(1).SelectMany(x => Task.FromResult(x.ToString()));
 
-            Console.WriteLine(ret);
+            var ret2 = from x in Task.FromResult(1)
+                       from y in Task.FromResult(2)
+                       select x + y;
+
+            var ret3 = from x in Task.FromResult(1)
+                       from y in Plus1(x)
+                       select y;
+
+            var ret5 = DivideZero();
+
+            Console.WriteLine(await ret1);
+            Console.WriteLine(await ret2);
+            Console.WriteLine(await ret3);
+
+            try
+            {
+                Console.WriteLine(await ret5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
+
+        private static Task<string> Plus1(int x) => Task.FromResult((x + 1).ToString());
+
+        private static Task<double> DivideZero() => Task.FromResult(DividZero());
+
+        private static double DividZero() => throw new NotImplementedException();
     }
 }
